4b02d6b4c8cf2c357a42a57f38f949f1f74e735c
Shravan Sunder <shravan.retail@gmail.com>
2021-05-24 10:32:57 -0400

    #1 jsx import order

--- src/components/routes/MainPage/MainPage.tsx
+++ src/components/routes/MainPage/MainPage.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@material-ui/core';
+import tw from 'twin.macro';
 
 export const MainPage = () => {
-   return <Button>new button</Button>;
+   return <Button css={tw`bg-red-500`}>new button</Button>;
 };
