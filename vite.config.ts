83acee9ac3043462858be733003ca3857edd4d55
Shravan Sunder <shravan.retail@gmail.com>
2021-05-24 10:25:57 -0400

    #1 aliases and types working

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
 import macrosPlugin from 'vite-plugin-babel-macros';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [reactRefresh(), macrosPlugin()],
+   plugins: [reactRefresh(), macrosPlugin(), tsconfigPaths()],
+
    esbuild: {
       jsxFactory: 'jsx',
       jsxInject: `import {jsx, css} from '@emotion/react'`,
@@ -13,6 +16,10 @@ export default defineConfig({
       'process.platform': JSON.stringify('win32'),
       'process.env': {},
    },
+   alias: {
+      '~~': resolve(__dirname, 'src'),
+   },
+
    // alias: {
    //    '@material-ui/icons': '@material-ui/icons/esm',
    // },
